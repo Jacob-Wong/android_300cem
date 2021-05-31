80c8e57f3104c98dec1803f490a8f9cb750e3215
Jacob-Wong <62383862+Jacob-Wong@users.noreply.github.com>
2021-05-31 16:18:03 +0800

    Created Function: login

--- app/src/main/java/edu/shape/android_300cem/StartActivity.java
+++ app/src/main/java/edu/shape/android_300cem/StartActivity.java
@@ -2,13 +2,41 @@ package edu.shape.android_300cem;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.firebase.auth.FirebaseUser;
 
 public class StartActivity extends AppCompatActivity {
+
+    Button login, register;
+
+    FirebaseUser firebaseUser;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_start);
+
+        login = findViewById(R.id.login);
+        register = findViewById(R.id.register);
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent loginIntent = new Intent(StartActivity.this, LoginActivity.class);
+                startActivity(loginIntent);
+            }
+        });
+
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent RegIntent = new Intent(StartActivity.this, RegisterActivity.class);
+                startActivity(RegIntent);
+            }
+        });
     }
 }
